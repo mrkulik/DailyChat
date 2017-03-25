75c9f957c37569ae4dcb8f2b78e5ec60599fcc81
Gleb Kulik <mr.kulik23@gmail.com>
2017-03-25 20:37:09 +0300

    Contact class.

---
+++ DailyChat/Contact.swift
@@ -0,0 +1,30 @@
+//
+//  Contact.swift
+//  DailyChat
+//
+//  Created by Gleb Kulik on 3/25/17.
+//  Copyright © 2017 Gleb Kulik. All rights reserved.
+//
+
+import Foundation
+
+class Contact {
+    
+    private var _name = ""
+    private var _id = ""
+    
+    init(id: String, name: String) {
+        _id = id
+        _name = name
+    }
+    
+    var name : String {
+        get {
+             return _name
+        }
+    }
+    
+    var id: String {
+        return _id
+    }
+}
