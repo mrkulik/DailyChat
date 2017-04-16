8113c78280824849abcb81d50c4651132f274cbd
Gleb Kulik <mr.kulik23@gmail.com>
2017-04-16 13:12:47 +0300

    Message cell created.

--- DailyChat/ChatViewController.swift
+++ DailyChat/ChatViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import JSQMessagesViewController
+import MobileCoreServices
+import AVKit
 
-class ChatViewController: UIViewController {
+class ChatViewController: JSQMessagesViewController {
 
+    private var messages = [JSQMessage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = super.collectionView(collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
